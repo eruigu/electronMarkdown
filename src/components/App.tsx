381d3817c35a479af46402e9c32ffbe30090bcb6
etr <erick@ruigu.dev>
2023-08-03 13:00:13 -0500

    Setting up electron interface

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="md:container mx-auto flex items-center justify-center h-max bg-gray-200">
+      <p className=" text-indigo-400 text-2xl">React + Tailwind + Typescript + Electron = ❤</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+function render() {
+  ReactDOM.render(<App />, document.getElementById("app"));
+}
+
+render();
+
